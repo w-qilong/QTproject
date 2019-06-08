f73eb8d25fc235c2cc39f57548cabc15fe68774c
wuqilong <1528625919@qq.com>
2019-06-08 17:25:18 +0800

    toolbar

--- menu_toobar_statusbar/Toolbar.py
+++ menu_toobar_statusbar/Toolbar.py
@@ -20,7 +20,24 @@ class Toolbar(QMainWindow):
         new=QAction(QIcon(r'D:\QTproject\Image\python_text_x_64px_8857_easyicon.net.png'),'new',self)
         tb1.addAction(new)
         open = QAction(QIcon(r'D:\QTproject\Image\python_text_x_64px_8857_easyicon.net.png'), 'open', self)
-        tb1.addAction(open )
+        tb1.addAction(open)
+        #可以设置同时显示文字和图标
+        tb1.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+
+        #可以创建多个菜单栏，他们会显示在同一行，但是采用不同的菜单栏进行区分
+        tb2 = self.addToolBar('file')
+        new = QAction(QIcon(r'D:\QTproject\Image\python_text_x_64px_8857_easyicon.net.png'), 'new', self)
+        tb2.addAction(new)
+        open = QAction(QIcon(r'D:\QTproject\Image\python_text_x_64px_8857_easyicon.net.png'), 'open', self)
+        tb2.addAction(open)
+        # 可以设置同时显示文字和图标
+        tb2.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+
+        tb1.actionTriggered.connect(self.toolbtpressed)
+        tb2.actionTriggered.connect(self.toolbtpressed)
+    def toolbtpressed(self,a):
+       print('被点击的是：',a.text())
+
 
 if __name__ == '__main__':
     app=QApplication (sys.argv)
